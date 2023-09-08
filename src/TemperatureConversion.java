a64343b876d27f17e6af1381db1f6ce7eb1f9c99
TFlynnFRCC <weegeeflower2002@gmail.com>
2023-09-08 17:05:51 -0600

    Update TemperatureConversion.java

--- src/TemperatureConversion.java
+++ src/TemperatureConversion.java
@@ -4,18 +4,18 @@ public class TemperatureConversion {
     public static void main(String[] args)
     {
         //Declaring Fahrenheit and initializing the Scanner
-        int Fahrenheit;
+        double fahrenheit;
         Scanner in = new Scanner(System.in);
 
         //User input prompt and input itself
         System.out.println("Please input the temperature in Fahrenheit");
-        Fahrenheit = in.nextInt();
+        fahrenheit = in.nextDouble();
 
         //mathematical operations for Celsius conversion, followed by C to Kelvin conversion
-        double Celsius = ((Fahrenheit - 32) * (5.0 / 9.0));
-        double Kelvin = (Celsius + 273.15);
+        double celsius = (fahrenheit - 32) * 5.0 / 9.0;
+        double kelvin = celsius + 273.15;
 
         //Output with both units of temperature
-        System.out.printf("The temperature in Celsius is %.2f degrees and is %.2f in Kelvin\n", Celsius, Kelvin);
+        System.out.printf("The temperature in Celsius is %.2f degrees and is %.2f in Kelvin\n", celsius, kelvin);
     }
 }
